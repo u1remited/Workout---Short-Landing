b15b37e1ecdc79474f8d1c4435c064b1c4eda3ca
Anton Valkovskyi <u1remited@gmail.com>
2022-10-27 12:50:33 +0300

    Creating burger, adding media rules for first screen + some changes

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,3 +3,21 @@ function scrollUp(){
     if(this.scrollY >= 1400) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll');
 }
 window.addEventListener('scroll', scrollUp);
+
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.nav__list'),
+    menuItem = document.querySelectorAll('.nav__item'),
+    hamburger = document.querySelector('.nav__burger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('nav__burger_active');
+        menu.classList.toggle('nav__list_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('nav__burger_active');
+            menu.classList.toggle('nav__list_active');
+        });
+    });
+});
