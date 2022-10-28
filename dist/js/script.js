dcb19ba450cdb87be3f479877125474ca434dc4c
Anton Valkovskyi <u1remited@gmail.com>
2022-10-28 09:17:04 +0300

    Media rules for Start section + some changes

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,8 +1,13 @@
+// Scroll Up
+
 function scrollUp(){
     const scrollUp = document.getElementById('scroll-up');
     if(this.scrollY >= 1400) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll');
 }
 window.addEventListener('scroll', scrollUp);
+
+
+// Burger menu
 
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.nav__list'),
